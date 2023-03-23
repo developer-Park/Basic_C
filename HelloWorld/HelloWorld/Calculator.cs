a8f9b23f2d4a7ddd7f2067ab7ce35d230057ac2c
developer-Park <83831110+developer-Park@users.noreply.github.com>
2023-03-23 18:07:01 +0900

    [feat] Calculator

---
+++ HelloWorld/HelloWorld/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HelloWorld
+{
+    class Calculator
+    {
+        public void addCal(int num1, int num2) {
+            Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        }
+        public void subCal(int num1, int num2)
+        {
+            Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        }
+
+    }
+
+
+}
